f80b5d8f5c48319714bfd54e794d1d0c168f09b4
Johan Burmester <johan.burmester@unikum.net>
2015-11-12 19:19:27 +0100

    outlines and a clock

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -12,6 +12,26 @@ define(function (require) {
     };
 
     $(document).ready(function () {
-        render();
+        startTime();
     });
+
+
+    this.startTime = function () {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = this.checkTime(m);
+        s = this.checkTime(s);
+        document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+
+    this.checkTime = function (i) {
+        if (i < 10) {
+            i = "0" + i
+        }
+        ;  // add zero in front of numbers < 10
+        return i;
+    }
 });
